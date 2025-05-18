c9ed569a47f16f4b7ccade4373868b2b96cb4a73
Abdul Samad <samadjs100@gmail.com>
2025-05-19 01:49:40 +0500

    env name updated

--- src/JS Files/Firebase.js
+++ src/JS Files/Firebase.js
@@ -1,16 +1,15 @@
-// firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FB_APIKEY,
-  authDomain: import.meta.env.VITE_FB_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FB_PROJECTID,
-  storageBucket: import.meta.env.VITE_FB_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_FB_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_FB_APPID,
-  measurementId: import.meta.env.VITE_FB_MEASUREMENTID,
+  apiKey: import.meta.env.VITE_FB_apiKey,
+  authDomain: import.meta.env.VITE_FB_authDomain,
+  projectId: import.meta.env.VITE_FB_projectId,
+  storageBucket: import.meta.env.VITE_FB_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FB_messagingSenderId,
+  appId: import.meta.env.VITE_FB_appId,
+  measurementId: import.meta.env.VITE_FB_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
